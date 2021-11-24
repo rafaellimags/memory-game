bb0a8da2f15ff104edfdb66cec743504fbd1ca14
Rafael Lima <rafaellimagms@gmail.com>
2021-11-24 19:37:18 -0300

    get flipped cards

--- src.js
+++ src.js
@@ -1,38 +1,64 @@
-for (let i = 1; i <= 12; i++) {
 
-    let flip = document.createElement("div")
-    flip.setAttribute("class", "flip-card")
-    flip.setAttribute("id", `card${i}`)
-    document.body.appendChild(flip)
+loadCards = function () {
 
-    let flipInner = document.createElement("div")
-    flipInner.setAttribute("class", "flip-card-inner")
-    flipInner.setAttribute("onclick", "flip(this)")
-    flip.appendChild(flipInner)
+    for (let i = 1; i <= 12; i++) {
 
-    let flipFront = document.createElement("div")
-    flipFront.setAttribute("class", "flip-card-front")
-    let flipBack = document.createElement("div")
-    flipBack.setAttribute("class", "flip-card-back")
-    flipInner.appendChild(flipFront)
-    flipInner.appendChild(flipBack)
+        const card = document.createElement("div")
+        card.setAttribute("class", "flip-card")
+        document.body.appendChild(card)
 
-    let cardFrontImg = document.createElement("div")
-    cardFrontImg.setAttribute("class", "card-img card-front-img")
-    let cardBackImg = document.createElement("div")
-    cardBackImg.setAttribute("class", "card-img card-back-img")
-    flipFront.appendChild(cardFrontImg)
-    flipBack.appendChild(cardBackImg)
+        const cardInner = document.createElement("div")
+        cardInner.setAttribute("id", `card${i}`)
+        cardInner.setAttribute("class", "flip-card-inner")
+        cardInner.setAttribute("onclick", "flip(this)")
+        card.appendChild(cardInner)
 
-    console.log(flip)
+        const cardFront = document.createElement("div")
+        cardFront.setAttribute("class", "flip-card-front")
+        const cardBack = document.createElement("div")
+        cardBack.setAttribute("class", "flip-card-back")
+        cardInner.appendChild(cardFront)
+        cardInner.appendChild(cardBack)
 
-}
+        const cardFrontImg = document.createElement("div")
+        cardFrontImg.setAttribute("class", "card-img card-front-img")
+        const cardBackImg = document.createElement("div")
+        cardBackImg.setAttribute("class", "card-img card-back-img")
+        cardFront.appendChild(cardFrontImg)
+        cardBack.appendChild(cardBackImg)
+
+        localStorage.clear()
+
+    }
+}()
 
 
 function flip(elem) {
-    elem.style.transform = `rotateY(${180}deg)`
+    elem.style.transform = 'rotateY(180deg)'
     const imgUrl = getComputedStyle(elem.querySelector(".flip-card-back").children.item(0)).backgroundImage
-    const imgName = imgUrl.split("/").pop().split(".").shift()
-    console.log(imgName)
+    // const imgName = imgUrl.split("/").pop().split(".").shift()
+    const rotated = []
 
+    document.querySelectorAll(".flip-card-inner").forEach(elem => {
+        if (elem.style.transform === 'rotateY(180deg)') rotated.push(elem.id) 
+    })
+
+    // const imgName = rotated.map(card => {
+    //    return getComputedStyle(card.querySelector(".flip-card-back").children.item(0)).backgroundImage
+    // })//.join("").split("/").pop().split(".").shift()
+
+    console.log(rotated)
+
+    /* USAR LOCAL STORAGE PARA GUARDAR PONTUAÇÃO */
+
+    // for (let i = 0; i <= localStorage.length; i++) {
+    //     if (localStorage.getItem(localStorage.key(i)) === imgName) {
+    //         alert("Iguais!")
+    //         localStorage.setItem("score")
+    //     } else {
+    //         elem.style.transform = 'rotateY(0deg)'
+    //     }
+    // }
+
+    // localStorage.setItem(elem.id, imgName)
 }
